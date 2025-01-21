0b4f21a84fb54dde97bf115774a443d267a5ff35
Andrey Bryantsev <bryansev.andrey@gmail.com>
2025-01-21 05:16:05 +0500

    add incremental models

---
+++ models/staging/flights/stg_flights__flights_merge.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'merge',
+        unique_key = ['flight_id'],
+        tags = ['flights'],
+        on_schema_change = 'sync_all_columns'
+    )
+}}
+SELECT
+    flight_id,
+    flight_no,
+    scheduled_departure,
+    scheduled_arrival,
+    departure_airport,
+    arrival_airport,
+    "status",
+    aircraft_code,
+    actual_departure,
+    actual_arrival
+FROM
+    {{ source('demo_src', 'flights') }}
+
+{% if is_incremental() %}
+WHERE
+    scheduled_departure <= (SELECT MAX(scheduled_departure) FROM {{ source('demo_src', 'flights') }}) - interval '100 day'
+{% endif %}
